999c89fe986f76b3ff7bd2bfc1f7954ce7e48ec4
ilhom <ilhommamatqulov276@gmail.com>
2023-03-02 13:30:18 +0500

    �

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import csv
 from tinydb import TinyDB
 from tinydb.database import Document
 db = TinyDB('db.json', indent=4)
 
+f=open('fruits.csv','r')
+data=csv.reader(f)
+for i in data:
+    key=i[0]
+    value=i[1]
+    doc={'name':f'{key}',
+         'price':f'{value}'}
+    db.insert(doc)
+    print(doc)
 
-# doc = {
-#     "name":"bananas",
-#     "price":12
-# }
-# db.insert(doc)
 print(db.all())
